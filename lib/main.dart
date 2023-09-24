7f0b0d985ca0b80919f08f37bc423077c2f7ef4f
Hysam <84386336+HR6002@users.noreply.github.com>
2023-09-24 06:56:02 +0100

    App loadUp Complete
    App loads up ok with a cool animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,242 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+
 
 
 main(){ runApp(const MaterialApp( 
 title: "Fats Track", 
 home: Login(),
 ));}
+
+
+class  Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+
+class Welcome_Screen extends StatefulWidget {
+  @override
+  _Welcome_ScreenState createState() => _Welcome_ScreenState();
+}
+
+class _Welcome_ScreenState extends State<Welcome_Screen> {
+  int _start = 4;
+
+  void startTimer() {
+    const oneSec = const Duration(seconds: 1);
+    Timer.periodic(
+      oneSec,
+      (Timer timer) => setState(
+        () {
+          if (_start < 1) {
+            timer.cancel();
+          } else {
+            _start = _start - 1;
+          }
+        },
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    startTimer();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var Login_signUp_Screen= Stack(
+            children: [
+              
+
+              Align(
+
+                alignment: AlignmentDirectional(0.00, 0.61),
+                child: TextButton(
+                  onPressed: () {
+                    print('Button pressed ...');
+                  },
+                  child: Text('LOGIN', style: TextStyle(
+                          fontFamily: 'Mukta',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black
+                        ),),
+                  style: TextButton.styleFrom(
+                    minimumSize: Size(275, 40),
+                    backgroundColor: Color(0xFFE0DBDB),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)
+                    ),
+                    
+                  
+                ),
+              ),),
+
+
+              Align(
+                alignment: AlignmentDirectional(0.00, 0.75),
+                child: TextButton(
+                  onPressed: () {
+                    print('Button pressed ...');
+                  },
+                  child: Text('SIGN UP', style: TextStyle(
+                          fontFamily: 'Mukta',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white
+                        ),),
+                  style: TextButton.styleFrom(
+                    minimumSize: Size(275, 40),
+                    backgroundColor: Color(0xFF3F0679),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)
+                    ),)),),
+
+
+             Align(
+                alignment: AlignmentDirectional(-1.30, -1.04),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    'Assets/images/Logo.png',
+                    width: 200,
+                    fit: BoxFit.cover,
+                  ),),),
+
+
+              Align(
+                alignment: AlignmentDirectional(-0.85, -0.48),
+                child: Text(
+                  'Hello!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                        fontFamily: 'Readex Pro',
+                        color: Colors.white,
+                        fontSize: 45,
+                        letterSpacing: 1.5,
+                      ),),),
+
+              Align(
+                alignment: AlignmentDirectional(-0.78, -0.29),
+                child: Text(
+                  'Welcome,',
+                  style: TextStyle(
+                        fontFamily: 'Readex Pro',
+                        color: Colors.white,
+                        fontSize: 45,
+                      ),),),            
+            
+              Align(
+                alignment: AlignmentDirectional(0.08, 0.44),
+                child: Text(
+                  'A world of effortless travel \nand adventure is waiting for you ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                        fontFamily: 'Readex Pro',
+                        color: Color(0xFF848181),
+                      ),),),
+            ],
+          );
+
+    var MainLogo =Image.asset('Assets/images/Logo_animated.gif', height: 440, fit: BoxFit.cover,);
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Stack(
+
+          children: <Widget>[
+
+
+
+            Visibility(
+              child: MainLogo,
+             visible: _start > 1 && _start <= 4 ? true : false,
+            ),
+          
+
+
+
+            Visibility(
+              child: Login_signUp_Screen,
+              visible: _start == 0 ? true : false,
+            ),
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -13,20 +245,43 @@ class Login extends StatelessWidget {
 
   
   Widget build(BuildContext context) {
-
-    var MainLogo =Image.asset('Assets/images/Untitled-2.png');
+    var hello=  Text("HELLO");
+    var MainLogo =Image.asset('Assets/images/Logo_animated.gif');
     var Login    = TextButton(onPressed: null, child:  Container( alignment: Alignment.center, child: Text("Log In", style: TextStyle(color: Colors.black, fontSize: 15, letterSpacing: 4), ),width: 140, height: 35,), style: TextButton.styleFrom(backgroundColor: Colors.white), );
-    var Sign_Up  =const OutlinedButton(onPressed: null, child: Text("Sign Up",),);
+    var Sign_Up  = OutlinedButton(onPressed: null, child: Text("Sign Up",),);
 
     var Button_Column=  Column(children: [Login, Sign_Up] ,);
 
+
+
+
     return  MaterialApp(
-    home: Scaffold(
-    backgroundColor: Colors.black,
-    body: Center(child:  Container(child: Button_Column),), 
+    home: Welcome_Screen()
 
 
 
 
 
-    ));}}
+    );}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
